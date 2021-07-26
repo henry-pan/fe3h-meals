ff9589fb3af04652776559a8486ec62d71e96184
Henry <henry-pan@users.noreply.github.com>
2021-07-26 14:26:18 -0700

    Refactor app to use hooks

--- bundle.js
+++ bundle.js
@@ -10,190 +10,145 @@
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */   "default": () => (/* binding */ App)
 /* harmony export */ });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var _data_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./data.js */ "./src/data.js");
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
 function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
 
 function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
 
 function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
 
 function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
 
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 
 
+function App() {
+  var _useState = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)([]),
+      _useState2 = _slicedToArray(_useState, 2),
+      selectedChars = _useState2[0],
+      setSelection = _useState2[1];
 
-var App = /*#__PURE__*/function (_React$Component) {
-  _inherits(App, _React$Component);
+  var handleCheckbox = function handleCheckbox(_char) {
+    var newChars = _toConsumableArray(selectedChars);
 
-  var _super = _createSuper(App);
+    if (newChars.includes(_char)) {
+      var index = newChars.indexOf(_char);
+      newChars.splice(index, 1);
+    } else {
+      newChars.push(_char);
+    }
 
-  function App(props) {
-    var _this;
+    setSelection(_toConsumableArray(newChars));
+  };
 
-    _classCallCheck(this, App);
+  var findMeals = function findMeals(char1, char2) {
+    var mealHash = {};
+    var sharedMeals = [];
+    _data_js__WEBPACK_IMPORTED_MODULE_1__.characters[char1]["meals"].forEach(function (meal) {
+      return mealHash[meal] = true;
+    });
+    _data_js__WEBPACK_IMPORTED_MODULE_1__.characters[char2]["meals"].forEach(function (meal) {
+      if (mealHash[meal]) sharedMeals.push(meal);
+    });
+    if (sharedMeals.length === 0) return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("li", null, "None!");
+    return sharedMeals.map(function (i) {
+      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("li", {
+        key: i
+      }, i);
+    });
+  };
 
-    _this = _super.call(this, props);
-    _this.state = {
-      selectedChars: []
-    };
-    return _this;
+  var mapChars = function mapChars(arr) {
+    var isPicked = selectedChars.length >= 2; // Hide support after picking second character
+
+    var isFirstPick = selectedChars[0] && !selectedChars[1];
+    return arr.map(function (i) {
+      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+        className: !selectedChars.includes(i) && isPicked ? "character disabled" : "character",
+        key: i
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("label", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("input", {
+        type: "checkbox",
+        onChange: function onChange() {
+          return handleCheckbox(i);
+        },
+        disabled: !selectedChars.includes(i) && isPicked
+      }), i, isFirstPick && _data_js__WEBPACK_IMPORTED_MODULE_1__.characters[selectedChars[0]]["supports"].includes(i) && /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("img", {
+        className: "support-icon",
+        src: "img/support.png"
+      })));
+    });
+  };
+
+  var eagles = ["Edelgard", "Hubert", "Ferdinand", "Linhardt", "Caspar", "Bernadetta", "Dorothea", "Petra"];
+  var lions = ["Dimitri", "Dedue", "Felix", "Ashe", "Sylvain", "Mercedes", "Annette", "Ingrid"];
+  var deer = ["Claude", "Lorenz", "Raphael", "Ignatz", "Lysithea", "Marianne", "Hilda", "Leonie"];
+  var church = ["Seteth", "Flayn", "Hanneman", "Manuela", "Gilbert", "Alois", "Catherine", "Shamir", "Cyril", "Jeritza", "Anna"];
+  var wolves = ["Yuri", "Balthus", "Constance", "Hapi"];
+  var mealSection;
+
+  if (selectedChars.length === 2) {
+    mealSection = /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("section", {
+      className: "meals"
+    }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h2", null, selectedChars[0], " and ", selectedChars[1], " both like:"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("ul", null, findMeals(selectedChars[0], selectedChars[1])));
   }
 
-  _createClass(App, [{
-    key: "handleCheckbox",
-    value: function handleCheckbox(_char) {
-      var newChars = _toConsumableArray(this.state.selectedChars);
-
-      if (newChars.includes(_char)) {
-        var index = newChars.indexOf(_char);
-        newChars.splice(index, 1);
-      } else {
-        newChars.push(_char);
-      }
-
-      this.setState({
-        selectedChars: _toConsumableArray(newChars)
-      });
-    }
-  }, {
-    key: "findMeals",
-    value: function findMeals(char1, char2) {
-      var mealHash = {};
-      var sharedMeals = [];
-      _data_js__WEBPACK_IMPORTED_MODULE_1__.characters[char1]["meals"].forEach(function (meal) {
-        return mealHash[meal] = true;
-      });
-      _data_js__WEBPACK_IMPORTED_MODULE_1__.characters[char2]["meals"].forEach(function (meal) {
-        if (mealHash[meal]) sharedMeals.push(meal);
-      });
-      if (sharedMeals.length === 0) return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("li", null, "None!");
-      return sharedMeals.map(function (i) {
-        return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("li", {
-          key: i
-        }, i);
-      });
-    }
-  }, {
-    key: "mapChars",
-    value: function mapChars(arr) {
-      var _this2 = this;
-
-      var selectedChars = this.state.selectedChars;
-      var isPicked = selectedChars.length >= 2;
-      var isFirstPick = selectedChars[0] && !selectedChars[1]; // Hides supports after picking second character
-
-      return arr.map(function (i) {
-        return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-          className: !selectedChars.includes(i) && isPicked ? "character disabled" : "character",
-          key: i
-        }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("label", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("input", {
-          type: "checkbox",
-          onChange: function onChange() {
-            return _this2.handleCheckbox(i);
-          },
-          disabled: !selectedChars.includes(i) && isPicked
-        }), i, isFirstPick && _data_js__WEBPACK_IMPORTED_MODULE_1__.characters[selectedChars[0]]["supports"].includes(i) && /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("img", {
-          className: "support-icon",
-          src: "img/support.png"
-        })));
-      });
-    }
-  }, {
-    key: "render",
-    value: function render() {
-      var eagles = ["Edelgard", "Hubert", "Ferdinand", "Linhardt", "Caspar", "Bernadetta", "Dorothea", "Petra"];
-      var lions = ["Dimitri", "Dedue", "Felix", "Ashe", "Sylvain", "Mercedes", "Annette", "Ingrid"];
-      var deer = ["Claude", "Lorenz", "Raphael", "Ignatz", "Lysithea", "Marianne", "Hilda", "Leonie"];
-      var church = ["Seteth", "Flayn", "Hanneman", "Manuela", "Gilbert", "Alois", "Catherine", "Shamir", "Cyril", "Jeritza", "Anna"];
-      var dlc = ["Yuri", "Balthus", "Constance", "Hapi"];
-      var mealSection;
-
-      if (this.state.selectedChars.length === 2) {
-        var meals;
-        meals = this.findMeals(this.state.selectedChars[0], this.state.selectedChars[1]);
-        mealSection = /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("section", {
-          className: "meals"
-        }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h2", null, this.state.selectedChars[0], " and ", this.state.selectedChars[1], " both like:"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("ul", null, meals));
-      }
-
-      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-        className: "content"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("header", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("img", {
-        src: "img/meal.png"
-      }), "Shared Meal Finder"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", null, "Click on two characters to determine which meals they both like."), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", null, "After clicking on the first character, an icon will appear on all characters they can support.")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("section", {
-        className: "character-tables-container"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-        className: "character-table"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h3", {
-        id: "be"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("span", {
-        className: "faction-banner",
-        id: "banner-be"
-      }), "Black Eagles"), this.mapChars(eagles)), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-        className: "character-table"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h3", {
-        id: "bl"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("span", {
-        className: "faction-banner",
-        id: "banner-bl"
-      }), "Blue Lions"), this.mapChars(lions)), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-        className: "character-table"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h3", {
-        id: "gd"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("span", {
-        className: "faction-banner",
-        id: "banner-gd"
-      }), "Golden Deer"), this.mapChars(deer)), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-        className: "character-table"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h3", {
-        id: "church"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("span", {
-        className: "faction-banner",
-        id: "banner-church"
-      }), "Church of Seiros"), this.mapChars(church)), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-        className: "character-table"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h3", {
-        id: "aw"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("span", {
-        className: "faction-banner",
-        id: "banner-aw"
-      }), "Ashen Wolves"), this.mapChars(dlc))), mealSection);
-    }
-  }]);
-
-  return App;
-}(react__WEBPACK_IMPORTED_MODULE_0__.Component);
-
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (App);
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "content"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("header", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("img", {
+    src: "img/meal.png"
+  }), "Shared Meal Finder"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", null, "Click on two characters to determine which meals they both like."), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", null, "After clicking on the first character, an icon will appear on all characters they can support.")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("section", {
+    className: "character-tables-container"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "character-table"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h3", {
+    id: "be"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("span", {
+    className: "faction-banner",
+    id: "banner-be"
+  }), "Black Eagles"), mapChars(eagles)), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "character-table"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h3", {
+    id: "bl"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("span", {
+    className: "faction-banner",
+    id: "banner-bl"
+  }), "Blue Lions"), mapChars(lions)), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "character-table"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h3", {
+    id: "gd"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("span", {
+    className: "faction-banner",
+    id: "banner-gd"
+  }), "Golden Deer"), mapChars(deer)), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "character-table"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h3", {
+    id: "church"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("span", {
+    className: "faction-banner",
+    id: "banner-church"
+  }), "Church of Seiros"), mapChars(church)), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "character-table"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h3", {
+    id: "aw"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("span", {
+    className: "faction-banner",
+    id: "banner-aw"
+  }), "Ashen Wolves"), mapChars(wolves))), mealSection);
+}
 
 /***/ }),
 
--- src/app.jsx
+++ src/app.jsx
@@ -1,108 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import { characters } from "./data.js";
 
-class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      selectedChars: []
-    }
-  }
 
+export default function App() {
+  const [selectedChars, setSelection] = useState([]);
 
-  handleCheckbox(char) {
-    const newChars = [...this.state.selectedChars];
+  const handleCheckbox = char => {
+    const newChars = [...selectedChars];
     if (newChars.includes(char)) {
       const index = newChars.indexOf(char);
       newChars.splice(index, 1);
     } else {
       newChars.push(char);
     }
-    this.setState({ selectedChars: [...newChars] });
-  }
+    setSelection([...newChars]);
+  };
 
 
-  findMeals(char1, char2) {
+  const findMeals = (char1, char2) => {
     const mealHash = {};
     const sharedMeals = [];
     characters[char1]["meals"].forEach(meal => mealHash[meal] = true);
-    characters[char2]["meals"].forEach(meal => { if (mealHash[meal]) sharedMeals.push(meal)} );
+    characters[char2]["meals"].forEach(meal => { if (mealHash[meal]) sharedMeals.push(meal) });
     if (sharedMeals.length === 0) return <li>None!</li>;
     return sharedMeals.map(i => <li key={i}>{i}</li>);
-  }
+  };
 
 
-  mapChars(arr) {
-    const { selectedChars } = this.state;
+  const mapChars = arr => {
     const isPicked = selectedChars.length >= 2;
-    const isFirstPick = selectedChars[0] && !selectedChars[1]; // Hides supports after picking second character
+    // Hide support after picking second character
+    const isFirstPick = selectedChars[0] && !selectedChars[1];
 
     return arr.map(i => 
       <div className={(!selectedChars.includes(i) && isPicked) ? "character disabled" : "character"} key={i}>
         <label>
           <input type="checkbox"
-            onChange={() => this.handleCheckbox(i)}
+            onChange={() => handleCheckbox(i)}
             disabled={!selectedChars.includes(i) && isPicked}/>
           {i}
           {isFirstPick && characters[selectedChars[0]]["supports"].includes(i) && <img className="support-icon" src="img/support.png" />}
         </label>
       </div>
     );
+  };
+
+
+  const eagles = ["Edelgard","Hubert","Ferdinand","Linhardt","Caspar","Bernadetta","Dorothea","Petra"];
+  const lions = ["Dimitri","Dedue","Felix","Ashe","Sylvain","Mercedes","Annette","Ingrid"];
+  const deer = ["Claude","Lorenz","Raphael","Ignatz","Lysithea","Marianne","Hilda","Leonie"];
+  const church = ["Seteth","Flayn","Hanneman","Manuela","Gilbert","Alois","Catherine","Shamir","Cyril", "Jeritza", "Anna"];
+  const wolves = ["Yuri","Balthus","Constance","Hapi"];
+
+  let mealSection;
+  if (selectedChars.length === 2){
+    mealSection = (
+      <section className="meals">
+        <h2>{selectedChars[0]} and {selectedChars[1]} both like:</h2>
+        <ul>{findMeals(selectedChars[0], selectedChars[1])}</ul>
+      </section>
+    );
   }
 
-
-  render() {
-    const eagles=["Edelgard","Hubert","Ferdinand","Linhardt","Caspar","Bernadetta","Dorothea","Petra"];
-    const lions=["Dimitri","Dedue","Felix","Ashe","Sylvain","Mercedes","Annette","Ingrid"];
-    const deer=["Claude","Lorenz","Raphael","Ignatz","Lysithea","Marianne","Hilda","Leonie"];
-    const church=["Seteth","Flayn","Hanneman","Manuela","Gilbert","Alois","Catherine","Shamir","Cyril", "Jeritza", "Anna"];
-    const dlc=["Yuri","Balthus","Constance","Hapi"];
-
-    let mealSection;
-    if (this.state.selectedChars.length === 2){
-      let meals;
-      meals = this.findMeals(this.state.selectedChars[0], this.state.selectedChars[1]);
-      mealSection = (
-        <section className="meals">
-          <h2>{this.state.selectedChars[0]} and {this.state.selectedChars[1]} both like:</h2>
-          <ul>{meals}</ul>
-        </section>
-      );
-    }
-
-    return (
-      <div className="content">
-        <header>
-          <h1><img src="img/meal.png" />Shared Meal Finder</h1>
-          <p>Click on two characters to determine which meals they both like.</p>
-          <p>After clicking on the first character, an icon will appear on all characters they can support.</p>
-        </header>
-        <section className="character-tables-container">
-          <div className="character-table">
-            <h3 id="be"><span className="faction-banner" id="banner-be" />Black Eagles</h3>
-            {this.mapChars(eagles)}
-          </div>
-          <div className="character-table">
-            <h3 id="bl"><span className="faction-banner" id="banner-bl" />Blue Lions</h3>
-            {this.mapChars(lions)}
-          </div>
-          <div className="character-table">
-            <h3 id="gd"><span className="faction-banner" id="banner-gd" />Golden Deer</h3>
-            {this.mapChars(deer)}
-          </div>
-          <div className="character-table">
-            <h3 id="church"><span className="faction-banner" id="banner-church" />Church of Seiros</h3>
-            {this.mapChars(church)}
-          </div>
-          <div className="character-table">
-            <h3 id="aw"><span className="faction-banner" id="banner-aw" />Ashen Wolves</h3>
-            {this.mapChars(dlc)}
-          </div>
-        </section>
-        {mealSection}
-      </div>
-    )
-  }
+  return (
+    <div className="content">
+      <header>
+        <h1><img src="img/meal.png" />Shared Meal Finder</h1>
+        <p>Click on two characters to determine which meals they both like.</p>
+        <p>After clicking on the first character, an icon will appear on all characters they can support.</p>
+      </header>
+      <section className="character-tables-container">
+        <div className="character-table">
+          <h3 id="be"><span className="faction-banner" id="banner-be" />Black Eagles</h3>
+          {mapChars(eagles)}
+        </div>
+        <div className="character-table">
+          <h3 id="bl"><span className="faction-banner" id="banner-bl" />Blue Lions</h3>
+          {mapChars(lions)}
+        </div>
+        <div className="character-table">
+          <h3 id="gd"><span className="faction-banner" id="banner-gd" />Golden Deer</h3>
+          {mapChars(deer)}
+        </div>
+        <div className="character-table">
+          <h3 id="church"><span className="faction-banner" id="banner-church" />Church of Seiros</h3>
+          {mapChars(church)}
+        </div>
+        <div className="character-table">
+          <h3 id="aw"><span className="faction-banner" id="banner-aw" />Ashen Wolves</h3>
+          {mapChars(wolves)}
+        </div>
+      </section>
+      {mealSection}
+    </div>
+  );
 }
-
-export default App;
